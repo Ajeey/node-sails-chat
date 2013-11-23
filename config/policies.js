2c6dd6ebf039cbdd051f69f65c59b425c8699a7f
Ajeey <ajey.charantimath@gmail.com>
2013-11-23 16:07:13 +0530

    Added policy for safety

--- config/policies.js
+++ config/policies.js
@@ -16,8 +16,8 @@ module.exports.policies = {
 
   // Default policy for all controllers and actions
   // (`true` allows public access) 
-  '*': true
-
+  '*': true,
+  'messages': 'MessagesPolicy'
   /*
 	// Here's an example of adding some policies to a controller
 	RabbitController: {
