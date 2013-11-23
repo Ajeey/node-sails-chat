dd2b8c6d9618e1db882413d74f1ababb436c31cd
Ajeey <ajey.charantimath@gmail.com>
2013-11-23 14:57:16 +0530

    Created login and signup pages

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -22,7 +22,28 @@ var MainController = {
   },
 
   signup: function (req, res) {
+    var username = req.param("username");
+    var password = req.param("password");
 
+    Users.findByUsername(username).done(function(err, usr){
+      if (err) {
+        res.send(500, { error: "DB ERROR" });
+      } else if (usr) {
+        res.send(400, { error: "Username is already taken " });
+      } else {
+        var hasher = require("password-hash");
+        password = hasher.generate(password);
+
+        Users.create({username: username, password: password}).done(function(error, user) {
+          if (error) {
+            res.send(500, {error: "DB Error"});
+          } else {
+            req.session.user = user;
+            res.send(user);
+          }
+        }
+      }
+    });
   },
 
   login: function (req, res) {
