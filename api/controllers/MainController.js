ac252b96270a4368d11708dbdaaa15bb396473ae
Ajeey <ajey.charantimath@gmail.com>
2013-11-23 15:03:59 +0530

    Added the controller code for views

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -47,7 +47,27 @@ var MainController = {
   },
 
   login: function (req, res) {
+    var username = req.param("username");
+    var password = req.param("password");
 
+    Users.findByUsername(username).done(function(err, usr){
+      if(err) {
+        res.send(500, {error: "DB Error"});
+      } else {
+        if (usr) {
+          var hasher = require("password-hash");
+          if (hasher.verify(password, usr.password)) {
+            req.session.user = usr;
+            res.send(usr);
+          } else {
+            res.send(400, { error: "Wrong password "});
+          }
+        } else {
+            res.send(404, { error: "User not found "});
+          }
+        }
+      }
+    });
   },
 
   chat: function (req, res) {
